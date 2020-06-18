87a71937902909d700ce94c8d44509f03fcef591
Viren Bhosale <virubhosale112@gmail.com>
2020-06-18 09:28:48 +0530

    Added Comment And Adapter with Detailed Activity

---
+++ app/src/main/java/com/example/gocorona/Comment.java
@@ -0,0 +1,21 @@
+package com.example.gocorona;
+
+public class Comment {
+
+    private  String content;
+
+    public Comment() {
+    }
+
+    public Comment(String content) {
+        this.content = content;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
